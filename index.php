ebbc881f7fbadb8e2ec1ad17f902be8ed2aff797
Eegpv <forwebdew124@mail.ru>
2024-05-29 23:04:57 +0300

    t

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
-<!-- // ↓ УРОК 3 С КАНАЛА "ПРОСТО О LARAVEL. CutCode" ↓ -->
-
-
 <?php
+
+// ↓ УРОК 3 С КАНАЛА "ПРОСТО О LARAVEL. CutCode" ↓ МАССИВЫ
+
+// ВНИМАНИЕ! КОД РАСКОМЕНТИРОВАН ДЛЯ НАГЛЯДНОСТИ, РАБОТАТЬ ПРИ ЗАПУСКЕ НЕ БУДЕТ
+// ПОКА НЕ ЗАКОМЕНТИРОВАННЫ ОТДЕЛНЫЕ УЧАСТКИ.
+
 // $arr = [
 //     'title' => 'some title',
 //     'some_count' => 22,
@@ -28,5 +31,25 @@
 // var_dump($array1);
 
 
-// ↓ УРОК 4 С КАНАЛА "ПРОСТО О LARAVEL. CutCode" ↓
+// ↓ УРОК 4 С КАНАЛА "ПРОСТО О LARAVEL. CutCode" ↓ ЦИКЛЫ
 
+// for ($i = 0; $i <10; $i++){
+//     echo $i . PHP_EOL;
+// }
+
+// $clients = file(__DIR__ . '/clients.txt');
+
+// $i = 0;
+// while ($i < count($clients)){
+//     echo $i . '---> ' . $clients[$i];
+//     $i++;
+// }
+// do{
+//     echo $i . ' ---> ' . $clients[$i];
+//     $i++;
+     
+// } while ($i < count($clients));
+
+// foreach ($clients as $key => $client){
+//         echo $key . ' ---> ' . $client;
+// }
