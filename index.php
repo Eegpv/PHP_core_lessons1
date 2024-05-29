6f768ed563a73f68eff1030a6d7c7d9aa760cb86
Eegpv <forwebdew124@mail.ru>
2024-05-29 22:17:56 +0300

    ё

--- index.php
+++ index.php
@@ -1,2 +1,32 @@
+<!-- // ↓ УРОК 3 С КАНАЛА "ПРОСТО О LARAVEL. CutCode" ↓ -->
+
+
 <?php
-$arr = [];
+// $arr = [
+//     'title' => 'some title',
+//     'some_count' => 22,
+//    'arr_inside_arr' => ['some1' => 1],
+//    'empty' => null,
+// ];
+// var_dump($arr);
+// echo "присваеваем результат переменной и возвращаем пораметр" . PHP_EOL;
+
+// $result = print_r($arr, true);
+// echo "выводим значение переменной" . PHP_EOL;
+// echo $result;
+
+// echo "удаляем элемент из массива и добавляем element x";
+
+// $array1 = [
+//     1 => 'element one',
+//     2 => 'element two',
+//     3 => 'element three',
+//     4 => 'element four',
+// ];
+// unset($array1[4]);
+// $array1[] = 'element x';
+// var_dump($array1);
+
+
+// ↓ УРОК 4 С КАНАЛА "ПРОСТО О LARAVEL. CutCode" ↓
+
